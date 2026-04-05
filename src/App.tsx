891681efdcd7efba8b55ca0207e3c0f6f56e54a4
Karlito <karelos101@gmail.com>
2026-04-05 20:37:41 +0200

    Implemented a basic sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import GraphView from "./GraphView";
+import Sidebar from "./Sidebar";
 
 function App() {
     return (
         <main className="w-screen h-screen overflow-hidden bg-[#16171C]">
-            <div className="w-full h-full">
-                <div className="h-1/5">
-                    <GraphView baseColor={"#9eff5d"} carNum={0} graphName="Throttle" nLines={3} type="throttle" />
+            <div className="flex gap-3 h-full">
+                <div className="">
+                    <Sidebar />
                 </div>
-                <div className="h-1/5">
-                    <GraphView baseColor={"#ff5d5d"} carNum={0} graphName="Brake" nLines={3} type="brake" />
-                </div>
-                <div className="h-1/5">
-                    <GraphView baseColor={"#5db1ff"} carNum={0} graphName="Engine RPM" nLines={3} type="rpm" />
-                </div>
-                <div className="h-1/5">
-                    <GraphView baseColor={"#cc5dff"} carNum={0} graphName="Delta" nLines={3} type="delta" />
-                </div>
-                <div className="h-1/5">
-                    <GraphView baseColor={"#efff5d"} carNum={0} graphName="Speed" nLines={3} type="speed" />
+                <div className=" h-full w-full">
+                    <div className="h-1/5 p-1 ">
+                        <GraphView baseColor={"#9eff5d"} carNum={0} graphName="Throttle" nLines={3} type="throttle" />
+                    </div>
+                    <div className="h-1/5 p-1">
+                        <GraphView baseColor={"#ff5d5d"} carNum={0} graphName="Brake" nLines={3} type="brake" />
+                    </div>
+                    <div className="h-1/5 p-1">
+                        <GraphView baseColor={"#5db1ff"} carNum={0} graphName="Engine RPM" nLines={3} type="rpm" />
+                    </div>
+                    <div className="h-1/5 p-1">
+                        <GraphView baseColor={"#cc5dff"} carNum={0} graphName="Delta" nLines={3} type="delta" />
+                    </div>
+                    <div className="h-1/5 p-1">
+                        <GraphView baseColor={"#efff5d"} carNum={0} graphName="Speed" nLines={3} type="speed" />
+                    </div>
                 </div>
             </div>
         </main>
