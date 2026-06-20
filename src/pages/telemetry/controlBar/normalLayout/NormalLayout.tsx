37dedbbe709e4950bb4bd13eceefcfa6d5ff8235
Karel Lukes <karelos101@gmail.com>
2026-06-20 17:58:48 +0200

    Refactored normal mode in graph view

--- src/pages/telemetry/controlBar/normalLayout/NormalLayout.tsx
+++ src/pages/telemetry/controlBar/normalLayout/NormalLayout.tsx
@@ -1,76 +1,13 @@
-import { useEffect, useState, useContext } from "react";
-import { invoke } from "@tauri-apps/api/core";
-import { TelemetryContext } from "../../Telemetry";
-import { MenuProps } from "antd";
-import DriverLayoutWidget from "./DriverLayoutsWidget";
+import { Separator } from "@/components/ui/separator";
 import DriverSelect from "./DriverSelect";
 import LayoutSelect from "./LayoutSelect";
 
 export default function NormalLayout() {
-  const [drivers, setDrivers] = useState<MenuProps["items"]>([]);
-  const [curDriver, setCurDriver] = useState("");
-  const [curLayout, setCurLayout] = useState("");
-  const c = useContext(TelemetryContext);
-  const layouts: MenuProps["items"] = [
-    ...c.layouts.map((data, i) => {
-      return { label: data.name, key: i.toString() };
-    }),
-    { type: "divider" },
-    {
-      key: "edit",
-      label: "edit",
-    },
-  ];
-  function handleLayout(key: string) {
-    if (key == "edit") {
-      c.setEditMode(true);
-    } else {
-      c.setActiveLayout(Number(key));
-      c.setGraphData(c.layouts[Number(key)].graphData);
-      c.setSizes(c.layouts[Number(key)].scales);
-      setCurLayout(c.layouts[Number(key)].name);
-    }
-  }
-
-  useEffect(() => {
-    invoke<Driver[]>("get_drivers").then((v) => {
-      try {
-        setCurDriver(v[c.curDriverNum].name);
-      } catch (e) {
-        console.error("No drivers found. The game is probably not runing.");
-        setCurDriver("N/A");
-      }
-      setCurLayout(c.layouts[c.activeLayout].name);
-      const items: MenuProps["items"] = v.map((driver) => ({
-        label: driver.name,
-        key: String(driver.index),
-      }));
-
-      setDrivers(items);
-    });
-  }, []);
   return (
     <>
-      {/* <DriverLayoutWidget
-        drivers={drivers}
-        layouts={layouts}
-        onDriverSelect={(key, driverName) => {
-          const carNum = Number(key);
-          setCurDriver(driverName);
-          console.log("Set car num ", carNum);
-          c.setCurDriverNum(carNum);
-        }}
-        onLayoutSelect={handleLayout}
-        curDriver={curDriver}
-        curLayout={curLayout}
-      /> */}
       <DriverSelect />
+      <Separator />
       <LayoutSelect />
     </>
   );
 }
-
-type Driver = {
-  index: number;
-  name: string;
-};
