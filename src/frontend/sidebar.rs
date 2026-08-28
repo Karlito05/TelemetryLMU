dd77ac7f733b85d5800ee35ff458098788f6292e
Karel Lukes <karelos101@gmail.com>
2026-08-28 19:50:20 +0200

    Minor changes in sidebar

--- src/frontend/sidebar.rs
+++ src/frontend/sidebar.rs
@@ -1,7 +1,7 @@
-use eframe::egui::{self, Color32, CornerRadius, pos2, vec2};
+use eframe::egui::*;
 use egui_phosphor_icons::{Icon, icons};
 
-use crate::frontend::frontend_main::Page;
+use crate::frontend::{frontend_main::Page, settings_page::Settings};
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
@@ -23,7 +23,7 @@ impl Default for Sidebar {
 }
 
 impl Sidebar {
-    pub fn draw_sidebar(&mut self, ui: &mut egui::Ui) {
+    pub fn draw_sidebar(&mut self, ui: &mut Ui, settings: &Settings) {
         // let screen_width = ui.ctx().viewport_rect().width();
         let sidebar_width = 300.0;
         let sidebar_height = ui.ctx().viewport_rect().height();
@@ -32,12 +32,12 @@ impl Sidebar {
             return;
         }
 
-        egui::Panel::left(egui::Id::new("e_sidebar"))
+        Panel::left(Id::new("e_sidebar"))
             .resizable(false)
             .exact_size(sidebar_width)
             .show_separator_line(false)
             .show(ui, |ui| {
-                let sidebar_back_rect = egui::Rect::from_min_size(
+                let sidebar_back_rect = Rect::from_min_size(
                     pos2(16.0, 16.0),
                     vec2(sidebar_width - 32.0, sidebar_height - 32.0),
                 );
@@ -47,7 +47,7 @@ impl Sidebar {
                     Color32::from_rgb(23, 23, 28),
                 );
 
-                ui.put(sidebar_back_rect, |ui: &mut egui::Ui| {
+                ui.put(sidebar_back_rect, |ui: &mut Ui| {
                     ui.vertical_centered_justified(|ui| {
                         ui.add_space(16.0);
 
@@ -59,7 +59,7 @@ impl Sidebar {
 
                         ui.add_space(ui.available_height() - 70.0);
 
-                        if self.draw_profile(ui).clicked() {
+                        if self.draw_profile(ui, settings).clicked() {
                             self.settings_open = true;
                         }
                     })
@@ -68,20 +68,20 @@ impl Sidebar {
             });
     }
 
-    fn draw_top(&self, ui: &mut egui::Ui) {
+    fn draw_top(&self, ui: &mut Ui) {
         ui.horizontal(|ui| {
             ui.spacing_mut().item_spacing.x = 16.0;
 
             ui.add_space(16.0);
 
             ui.add(
-                egui::Image::new(egui::include_image!("../../public/Logo.svg"))
+                Image::new(include_image!("../../public/Logo.svg"))
                     .fit_to_exact_size(vec2(48.0, 48.0)),
             );
             ui.add(
-                egui::Label::new(
-                    egui::RichText::new("Telemetry LMU")
-                        .family(egui::FontFamily::Name("RacingSansOne".into()))
+                Label::new(
+                    RichText::new("Telemetry LMU")
+                        .family(FontFamily::Name("RacingSansOne".into()))
                         .size(24.0)
                         .color(Color32::WHITE),
                 )
@@ -90,13 +90,13 @@ impl Sidebar {
         });
     }
 
-    fn draw_buttons(&mut self, ui: &mut egui::Ui) {
+    fn draw_buttons(&mut self, ui: &mut Ui) {
         ui.horizontal(|ui| {
             ui.add_space(16.0);
             ui.vertical(|ui| {
-                ui.add(egui::Label::new(
-                    egui::RichText::new("Live Analysis")
-                        .family(egui::FontFamily::Name("DaysOne".into()))
+                ui.add(Label::new(
+                    RichText::new("Live Analysis")
+                        .family(FontFamily::Name("DaysOne".into()))
                         .size(12.0)
                         .color(Color32::from_white_alpha(127)),
                 ));
@@ -129,9 +129,9 @@ impl Sidebar {
 
                 ui.add_space(32.0);
 
-                ui.add(egui::Label::new(
-                    egui::RichText::new("Reflect")
-                        .family(egui::FontFamily::Name("DaysOne".into()))
+                ui.add(Label::new(
+                    RichText::new("Reflect")
+                        .family(FontFamily::Name("DaysOne".into()))
                         .size(12.0)
                         .color(Color32::from_white_alpha(127)),
                 ));
@@ -153,27 +153,21 @@ impl Sidebar {
         });
     }
 
-    fn draw_button(
-        &self,
-        ui: &mut egui::Ui,
-        text: String,
-        icon: Icon,
-        active: bool,
-    ) -> egui::Response {
-        let desired_size = egui::vec2(234.0, 36.0);
+    fn draw_button(&self, ui: &mut Ui, text: String, icon: Icon, active: bool) -> Response {
+        let desired_size = vec2(234.0, 36.0);
 
-        let (rect, response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
+        let (rect, response) = ui.allocate_exact_size(desired_size, Sense::click());
 
         let bg_color = if active || response.hovered() {
-            egui::Color32::from_rgb(22, 23, 28).blend(Color32::from_white_alpha(255 / 20)) //5% white
+            Color32::from_rgb(22, 23, 28).blend(Color32::from_white_alpha(255 / 20)) //5% white
         } else {
-            egui::Color32::from_rgb(22, 23, 28)
+            Color32::from_rgb(22, 23, 28)
         };
 
         ui.painter()
-            .rect_filled(rect, egui::CornerRadius::same(8), bg_color);
+            .rect_filled(rect, CornerRadius::same(8), bg_color);
 
-        ui.put(rect, |ui: &mut egui::Ui| {
+        ui.put(rect, |ui: &mut Ui| {
             ui.horizontal(|ui| {
                 ui.add_space(2.0);
                 ui.label(
@@ -181,7 +175,7 @@ impl Sidebar {
                         .size(24.0)
                         .color(Color32::from_rgb(19, 141, 241)),
                 );
-                ui.label(egui::RichText::new(text).size(16.0).color(Color32::WHITE));
+                ui.label(RichText::new(text).size(16.0).color(Color32::WHITE));
             })
             .response
         });
@@ -189,10 +183,10 @@ impl Sidebar {
         response
     }
 
-    fn draw_profile(&self, ui: &mut egui::Ui) -> egui::Response {
-        let desired_size = egui::vec2(ui.available_width() - 16.0, 64.0);
+    fn draw_profile(&self, ui: &mut Ui, settings: &Settings) -> Response {
+        let desired_size = vec2(ui.available_width() - 16.0, 64.0);
 
-        let (rect, response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
+        let (rect, response) = ui.allocate_exact_size(desired_size, Sense::click());
 
         // Background
         let bg_color = if response.hovered() {
@@ -202,35 +196,34 @@ impl Sidebar {
         };
 
         ui.painter()
-            .rect_filled(rect, egui::CornerRadius::same(16), bg_color);
+            .rect_filled(rect, CornerRadius::same(16), bg_color);
 
         // Profile image
-        let image_rect =
-            egui::Rect::from_min_size(rect.min + egui::vec2(8.0, 8.0), egui::vec2(48.0, 48.0));
+        let image_rect = Rect::from_min_size(rect.min + vec2(8.0, 8.0), vec2(48.0, 48.0));
 
         ui.put(
             image_rect,
-            egui::Image::new(egui::include_image!("../../public/Logo.svg"))
-                .fit_to_exact_size(egui::vec2(48.0, 48.0))
-                .corner_radius(egui::CornerRadius::same(8)),
+            Image::new(include_image!("../../public/Logo.svg"))
+                .fit_to_exact_size(vec2(48.0, 48.0))
+                .corner_radius(CornerRadius::same(8)),
         );
 
         // Name
         ui.painter().text(
-            egui::pos2(rect.min.x + 80.0, rect.center().y),
-            egui::Align2::LEFT_CENTER,
-            "Test Acc",
-            egui::FontId::proportional(24.0),
+            pos2(rect.min.x + 80.0, rect.center().y),
+            Align2::LEFT_CENTER,
+            settings.name.clone(),
+            FontId::proportional(24.0),
             Color32::WHITE,
         );
 
         // More icon
         ui.put(
-            egui::Rect::from_min_size(
-                egui::pos2(rect.max.x - 40.0, rect.center().y - 16.0),
-                egui::vec2(24.0, 32.0),
+            Rect::from_min_size(
+                pos2(rect.max.x - 40.0, rect.center().y - 16.0),
+                vec2(24.0, 32.0),
             ),
-            egui::Label::new(
+            Label::new(
                 icons::DOTS_THREE_VERTICAL
                     .bold()
                     .size(24.0)
