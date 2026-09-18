38378a5dc6519cd2e71545e698935fb28ef7b72a
Karel Lukes <karelos101@gmail.com>
2026-09-18 17:33:54 +0200

    implemented input UI in the car info page

--- src/frontend/car_info_page.rs
+++ src/frontend/car_info_page.rs
@@ -453,6 +453,105 @@ impl CarInfo {
             CornerRadius::same(16),
             Color32::from_white_alpha(17),
         );
+        let usable_rect = Rect::from_min_max(
+            input_rect.min + vec2(16.0, 16.0),
+            input_rect.max - vec2(16.0, 16.0),
+        );
+
+        ui.put(usable_rect, |ui: &mut Ui| {
+            ui.vertical(|ui| {
+                ui.horizontal(|ui| {
+                    ui.label(
+                        egui_phosphor_icons::icons::TIRE
+                            .regular()
+                            .size(14.0)
+                            .color(Color32::from_white_alpha(64)),
+                    );
+                    ui.label(
+                        RichText::new("INPUT")
+                            .size(12.0)
+                            .family(FontFamily::Name("BarlowCondensed".into()))
+                            .color(Color32::from_white_alpha(64)),
+                    );
+                });
+
+                ui.add_space(16.0);
+
+                ui.label(
+                    RichText::new("Throttle")
+                        .size(16.0)
+                        .family(FontFamily::Name("JetBrainsMono".into()))
+                        .color(Color32::WHITE),
+                );
+                ui.allocate_ui(vec2(ui.available_width(), 12.0), |ui| {
+                    ui.painter()
+                        .rect_filled(ui.max_rect(), 8.0, Color32::from_white_alpha(25));
+                    ui.painter().rect_filled(
+                        Rect::from_min_size(
+                            ui.max_rect().min,
+                            vec2(ui.max_rect().size().x * 0.6, ui.max_rect().size().y),
+                        ),
+                        8.0,
+                        Color32::GREEN,
+                    );
+                });
+                ui.add_space(40.0);
+
+                ui.label(
+                    RichText::new("Brake")
+                        .size(16.0)
+                        .family(FontFamily::Name("JetBrainsMono".into()))
+                        .color(Color32::WHITE),
+                );
+                ui.allocate_ui(vec2(ui.available_width(), 12.0), |ui| {
+                    ui.painter()
+                        .rect_filled(ui.max_rect(), 8.0, Color32::from_white_alpha(25));
+                    ui.painter().rect_filled(
+                        Rect::from_min_size(
+                            ui.max_rect().min,
+                            vec2(ui.max_rect().size().x * 0.3, ui.max_rect().size().y),
+                        ),
+                        8.0,
+                        Color32::RED,
+                    );
+                });
+
+                ui.add_space(40.0);
+
+                ui.label(
+                    RichText::new("Steering")
+                        .size(16.0)
+                        .family(FontFamily::Name("JetBrainsMono".into()))
+                        .color(Color32::WHITE),
+                );
+                let input = 1.0;
+                ui.allocate_ui(vec2(ui.available_width(), 12.0), |ui| {
+                    ui.painter()
+                        .rect_filled(ui.max_rect(), 8.0, Color32::from_white_alpha(25));
+                    ui.painter().rect_filled(
+                        Rect::from_min_max(
+                            pos2(
+                                ui.max_rect().min.x
+                                    + ui.max_rect().size().x
+                                        * if input > 0.0 { 1.0 } else { 1.0 + input }
+                                        / 2.0,
+                                ui.max_rect().min.y,
+                            ),
+                            pos2(
+                                ui.max_rect().min.x
+                                    + ui.max_rect().size().x
+                                        * if input < 0.0 { 1.0 } else { 1.0 + input }
+                                        / 2.0,
+                                ui.max_rect().max.y,
+                            ),
+                        ),
+                        8.0,
+                        Color32::CYAN,
+                    );
+                });
+            })
+            .response
+        });
     }
 
     fn draw_fuel_panel(&self, ui: &mut Ui, fuel_rect: Rect) {
